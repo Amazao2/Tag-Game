3a81b1e43119d7f36546136887ef607a38a78003
Brandon McRae <bmcrae2@uwo.ca>
2014-11-28 10:50:11 -0500

    Basic Navigation
    - basic follow script attached to troll
    - nav baked from scene

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform player;
+    NavMeshAgent nav;
+
+
+    void Awake ()
+    {
+        nav = GetComponent <NavMeshAgent> ();
+    }
+
+
+    void Update ()
+    {
+        //if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+        //{
+            nav.SetDestination (player.position);
+        //}
+        //else
+        //{
+        //    nav.enabled = false;
+        //}
+    }
+}
